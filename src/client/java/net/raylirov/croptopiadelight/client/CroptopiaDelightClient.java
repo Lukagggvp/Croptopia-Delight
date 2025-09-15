1284ab8a694042ce41abd08eeb849e6d2c7d0245
Ropyxa <118303452+Lukagggvp@users.noreply.github.com>
2025-09-15 23:24:16 +0300

    m4

---
+++ src/client/java/net/raylirov/croptopiadelight/client/CroptopiaDelightClient.java
@@ -0,0 +1,10 @@
+package net.raylirov.croptopiadelight.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CroptopiaDelightClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/raylirov/croptopiadelight/CroptopiaDelight.java
@@ -0,0 +1,10 @@
+package net.raylirov.croptopiadelight;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CroptopiaDelight implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
